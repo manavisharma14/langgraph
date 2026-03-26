18d4aaec835bf0c3fda7d7477c49e4dbea5d79c9
manavisharma14 <manavisharma14@gmail.com>
2026-03-26 08:01:32 -0500

    crash workflow + langgraph_chatbot

--- langgraph_chatbot/streamlit_frontend.py
+++ langgraph_chatbot/streamlit_frontend.py
@@ -1,30 +1,45 @@
+# 
+
 import streamlit as st
 from langgraph_backend import chatbot
 from langchain_core.messages import HumanMessage
 
-CONFIG = {'configurable' : {'thread_id' : 'thread-1'}}
+CONFIG = {"configurable": {"thread_id": "thread-1"}}
 
-# st.session_state -> dict (doesn't erase)
+# place to store messages: st.session_state
+if 'messages' not in st.session_state:
+    st.session_state.messages = []
 
-if 'message_history' not in st.session_state:
-    st.session_state['message_history'] = []
+for msg in st.session_state.messages:
+    with st.chat_message(msg['role']):
+        st.write(msg['content'])
 
-for message in st.session_state['message_history']: 
-    with st.chat_message(message['role']):
-        st.text(message['content'])
-
-user_input = st.chat_input('type here ')
+user_input = st.chat_input("type here!")
 
 if user_input:
+    st.session_state.messages.append({
+        "role": "user",
+        "content": user_input
+    })
 
-    # first add message to message_history
-    st.session_state['message_history'].append({'role': 'user', 'content': user_input})
-    with st.chat_message('user'):
-        st.text(user_input)
+    # display user message
+    with st.chat_message("user"):
+        st.write(user_input)
 
-    response = chatbot.invoke({'messages' : [HumanMessage(content=user_input)]}, config=CONFIG)
-    ai_message = response['messages'][-1].content
+    # call langgraph backend
+    response = chatbot.invoke(
+        {"messages": [HumanMessage(content=user_input)]},
+        config=CONFIG
+    )
 
-    st.session_state['message_history'].append({'role': 'assistant', 'content': ai_message})
-    with st.chat_message('assistant'):
-        st.text(ai_message)
+    # EXTRACT AI message
+    ai_message = response["messages"][-1].content
+
+    # store ai message
+    st.session_state.messages.append({
+        "role": "assistant",
+        "content": ai_message 
+    })
+
+    with st.chat_message("assistant"):
+        st.write(ai_message)
